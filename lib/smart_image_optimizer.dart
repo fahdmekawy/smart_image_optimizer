20222f401503b2ee1b91b54db2f4bc70d9df5918
fahdmekawy <fahd.mekawy@swypex.com>
2025-08-16 12:13:28 +0300

    configs

--- lib/smart_image_optimizer.dart
+++ lib/smart_image_optimizer.dart
@@ -1,5 +1,3 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library smart_image_optimizer;
+
+export 'src/optimizer.dart' show SmartImageOptimizer, ImageFormat;
